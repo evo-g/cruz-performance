01875220a531a7413ce24a5740541bb04568f6b5
Evodio Garcia <evodiog@gmail.com>
2020-04-28 18:35:43 -0700

    corrects outgoing text message placeholder

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -7,7 +7,7 @@ export default function Pricing() {
       <h2>Call for Pricing</h2>
       <a href="tel:6194024165">Call us at 619-402-4165</a>
       <h2>Or Text</h2>
-      <a href="sms:+16194024165&amp;body=Hi%2520there%252C%2520I%2527d%2520like%2520to...">Click here to text us!</a>
+      <a href="sms:+16194024165">Click here to text us!</a>
     </StyledPricing>
   )
 }
